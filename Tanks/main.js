9f4ac39788bd4aeed33ba9ded96ae6880deb6eed
qertend <113005672+qertend@users.noreply.github.com>
2022-10-19 20:18:08 +0200

    Add files via upload

--- Tanks/main.js
+++ Tanks/main.js
@@ -9,24 +9,33 @@ let redY = canvas.height/2-redTank.height/2; // places starting Y coordinates in
 let redRotation = 0;
 let keyBuffer = {};
 
+//initialize event listeners
 window.addEventListener("keydown", keyDown);
 window.addEventListener("keyup", keyDown);
 window.addEventListener("keydown", renderFrame);
 
+// render first frame
+renderFrame();
+
 function redConvToXY(direction) { //converts the tanks movements to X and Y coordinates
-    redX += direction * Math.sin(-redRotation*Math.PI/180);
-    redY += direction * Math.cos(-redRotation*Math.PI/180);
+    if (redX + direction * Math.sin(-redRotation*Math.PI/180) < canvas.width && redX + direction * Math.sin(-redRotation*Math.PI/180) > 0) {
+        redX += direction * Math.sin(-redRotation*Math.PI/180);
+    }   
+    if (redY + direction * Math.cos(-redRotation*Math.PI/180) < canvas.height && redY + direction * Math.cos(-redRotation*Math.PI/180) > 0) {
+        redY += direction * Math.cos(-redRotation*Math.PI/180);
+    }
 }
 
 function keyDown(event) {
     keyBuffer[event.keyCode] = event.type == "keydown"; // checks if the key in 'event' is down or up
-    if (keyBuffer[87]) { redConvToXY(-3);} // W
-    if (keyBuffer[65]) { redRotation-=2;} // A
-    if (keyBuffer[83]) { redConvToXY(3);} // S
-    if (keyBuffer[68]) { redRotation+=2;} // D
 }
 
 function renderFrame() { // renders the current frame when called
+    requestAnimationFrame(renderFrame)
+    if (keyBuffer[87]) { redConvToXY(-0.01);} // W
+    if (keyBuffer[65]) { redRotation-=0.01;} // A
+    if (keyBuffer[83]) { redConvToXY(0.01);} // S
+    if (keyBuffer[68]) { redRotation+=0.01;} // D
     ctx.setTransform(1, 0, 0, 1, 0, 0); // resets rotation and translate
     ctx.clearRect(0,0, canvas.width, canvas.height); // resets image on canvas
     ctx.translate(redX, redY); // places 0,0 at tank
@@ -36,5 +45,5 @@ function renderFrame() { // renders the current frame when called
     ctx.beginPath();//yellow-
     ctx.ellipse(0, 0, 5, 5, 0, 0, Math.PI * 2);//debug-
     ctx.fill();//circle]
-
 }
+
