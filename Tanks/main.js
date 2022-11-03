f935a4329b194fb63cf6f6c8428f71544119a06c
qertend <szucslevente607@gmail.com>
2022-11-03 18:30:39 +0100

    .

--- Tanks/main.js
+++ Tanks/main.js
@@ -4,8 +4,6 @@ const canvasLab = document.getElementById('labyrinthGen');
 const ctxLab = canvasLab.getContext("2d");
 let grid = 8; // number of rows and coloumns in labyrinth, will be modifiable by user
 let speed = 0.3; // sets the movement and turn speed of the tanks, will be modifiable by user
-let p1Bullets = new Set();
-let p2Bullets = new Set();
 let bulletLifetime = 3500; // bullet lifetime in milliseconds
 let maxBulletCount = 3;
 let keyBuffer = {};
@@ -62,19 +60,41 @@ class Tank {
                 this.bullets.delete(x);
             }
         }
+        if (keyBuffer[this.keyForward]) { this.move(-speed);} // Forwards
+        if (keyBuffer[this.keyBackward]) { this.move(speed);} // Backwards
+        if (keyBuffer[this.keyLeft]) { this.rotate(-speed);} // Left
+        if (keyBuffer[this.keyRight]) { this.rotate(speed);} // Right
+        //horizontal pos in grid Math.floor(this.x / (canvas.width/grid))
+        //vertical pos in grid Math.floor(this.y / (canvas.height/grid))
 
         this.move();
+    }
+
+    check() {
+        for (let x in []) {
+            if (mcd() == [0, 0]) {
+
+            }
+
+        }
     }
     shoot() {
         if (this.bullets.size < maxBulletCount) {
             this.bullets.add(new Bullet(this.rotation));
         }
     }
-    move() {
-        if (keyBuffer[this.keyForward]) { redMovement(-speed);} // Forwards
-        if (keyBuffer[this.keyBackward]) { redMovement(speed);} // Backwards
-        if (keyBuffer[this.keyLeft]) { this.rotation-=speed;} // Left
-        if (keyBuffer[this.keyRight]) { this.rotation+=speed;} // Right
+    move(speed) {
+        // checks if out of bounds on X axis
+        if (this.x + direction * Math.sin(-redTank.rotation*Math.PI/180) < canvas.width && this.x + direction * Math.sin(-redTank.rotation*Math.PI/180) > 0 && this.check()) {
+            this.x += direction * Math.sin(-redTank.rotation*Math.PI/180);
+        }
+        // checks if out of bounds on Y axis
+        if (this.y + direction * Math.cos(-redTank.rotation*Math.PI/180) < canvas.height && this.y + direction * Math.cos(-redTank.rotation*Math.PI/180 ) > 0 && this.check()) {
+            this.y += direction * Math.cos(-redTank.rotation*Math.PI/180);
+        }
+    }
+    rotate(speed) {
+
     }
 }
 
@@ -83,7 +103,151 @@ redTankImg.src = "redTank.png";
 redTankImg.width /= grid*1.5; // adjusts Tank image X size to half of grid size
 redTankImg.height /= grid*1.5; // adjusts Tank image Y size to half of grid size
 
-redTank = new Tank(redTankImg.width, redTankImg.height, 400, 400, "KeyW", "KeyS", "KeyA", "KeyD", "Space"); // replace static values with variables
+redTank = new Tank(redTankImg.width, redTankImg.height, 350, 350, "KeyW", "KeyS", "KeyA", "KeyD", "Space"); // replace static values with variables e.g. p1Forward
+
+//DO NOT ASK HOW IT WORKS, IT JUST DOES. I SPENT WAY TOO MUCH TIME MAKING IT
+function mcd(direction, width_, height_, rotation, rectCoord, x_) { //mcd stands for Magic Collision Detector
+    //Input str:"v" for vertical, str:"h" for horizontal collision points 
+    //x_ : offset between collision line and rectangle center point
+    let rotation_, // rectangle angle of rotation in degrees
+    l1, //collision point one (return 0 if no collision is detected)
+    l2, //collision point two (same here)
+    l; //distance betwwen l1 and l2 (isn't returned)
+    let mirror = false; //true if x_ is negative
+    switch (direction) {
+        case "v":
+            //rectCoord is Y
+            rotation_ = (rotation % 360 + 360) % 360;
+            break;
+        case "h":
+            //rectCoord is X
+            rotation_ = (rotation % 360 + 450) % 360;
+            break;
+        default:
+            console.error("incorrect direction: " + direction);
+            return;
+        }
+
+    if (x_ < 0) {
+        mirror = true;
+        x_ = -x_;
+    }
+    // at angles 0 or 180
+    if (rotation_ == 0 || rotation_ == 180) {
+        if (x_ <= width_/2) {
+            l = height_;
+            l1 = rectCoord - l/2;
+            l2 = l1 + l;
+        }
+        else {
+            l = -1;
+            l1 = 0;
+            l2 = 0;
+        }
+    }
+    // at angles 0 - 90 OR angles 180 - 270
+    else if (rotation_ < 90 || (rotation_ > 180 && rotation_ < 270)) {
+        rotationRad = (rotation_ % 180)*Math.PI/180;
+        l = (height_/2-(x_)/Math.sin(rotationRad)+width_/2/Math.tan(rotationRad))/Math.cos(rotationRad);
+        // across adjacent walls
+        if (l*Math.sin(rotationRad) < width_) {
+            if (x_ < width_/2 && l > Math.cos(rotationRad)*height_) { // across parallel walls
+                l = height_/Math.cos(rotationRad);
+                l1 = rectCoord - (l/2 + x_*Math.tan(rotationRad));
+                l2 = l1 + l;
+            }
+            else { //actually across adjacent walls
+                l1 = rectCoord-((width_/Math.sin(rotationRad))/2-(((x_)/Math.sin(rotationRad))*Math.cos(rotationRad)));
+                l2 = l1 + l;
+            }
+        }
+        // across parallel walls
+        else {
+            l = width_/Math.sin(rotationRad);
+            l1 = rectCoord-((l/2)-(((x_)/Math.sin(rotationRad))*Math.cos(rotationRad)));
+            l2 = l1 + l;
+        }
+    }
+    // at angles 90 or 270
+    else if (rotation_ == 90 || rotation_ == 270) {
+        if (x_ <= height_/2) {
+            l = width_;
+            l1 = rectCoord - l/2;
+            l2 = l1 + l;
+        }
+        else {
+            l = -1;
+            l1 = 0;
+            l2 = 0;
+        }
+    }
+    // at angles 90 - 180 OR angles 270 - 360
+    else if ((rotation_ > 90 && rotation_ < 180) || (rotation_ > 270 && rotation_ < 360)) {
+        rotationRad = ((rotation_ % 180)-90)*Math.PI/180;
+        l = (width_/2-(x_)/Math.sin(rotationRad)+height_/2/Math.tan(rotationRad))/Math.cos(rotationRad);
+        // across adjacent walls
+        if (l*Math.cos(rotationRad) < width_) {
+            if (x_ < width_/2 && l > Math.sin(rotationRad)*height_) { // across parallel walls
+                l = height_/Math.sin(rotationRad);
+                l1 = rectCoord - (l/2 - x_/Math.tan(rotationRad));
+                l2 = l1 + l;
+            }
+            else { //actually across adjacent walls
+                l1 = rectCoord-((height_/Math.sin(rotationRad))/2-(((x_)/Math.sin(rotationRad))*Math.cos(rotationRad)));
+                l2 = l1 + l;
+            }
+        }
+        // across parallel walls
+        else {
+            rotationRad = (rotation_ % 180)*Math.PI/180;
+            l = width_/Math.sin(rotationRad);
+            l1 = rectCoord - ((l/2)-(((x_)/Math.sin(rotationRad))*Math.cos(rotationRad)));
+            l2 = l1 + l;
+        }
+    }
+
+    if (mirror) {
+        l1 -= 2*(l1-rectCoord);
+        l2 -= 2*(l2-rectCoord);
+    }
+
+    if (direction == "v") {
+        //data for debug menu
+        document.getElementById('l1V').innerHTML = l1;
+        document.getElementById('l2V').innerHTML = l2;
+        document.getElementById('lV').innerHTML = l;
+        document.getElementById('distanceV').innerHTML = x_;
+        // COLLISION DETECT
+        if (l < 0) {
+            document.getElementById('noCollisionVertical').innerHTML = "No collision on vertical axis";
+            return [0,0];
+        }
+        else {
+            document.getElementById('noCollisionVertical').innerHTML = "";
+            return [l1, l2];
+        }
+    }
+    else if (direction == "h") {
+        //data for debug menu
+        document.getElementById('l1H').innerHTML = l1;
+        document.getElementById('l2H').innerHTML = l2;
+        document.getElementById('lH').innerHTML = l;
+        document.getElementById('distanceH').innerHTML = x_;
+        //it works like tihs, cause math
+        l1 -= 2*(l1-rectCoord);
+        l2 -= 2*(l2-rectCoord);
+        // COLLISION DETECT
+        if (l < 0) {
+            document.getElementById('noCollisionHorizontal').innerHTML = "No collision on horizontal axis";
+            return [0, 0];
+        }
+        else {
+            document.getElementById('noCollisionHorizontal').innerHTML = "";
+            return [l1, l2];
+        }
+    }
+}
+
 
 function generateLabyrinth() {
     ctxLab.clearRect(0,0,canvasLab.width, canvasLab.height); // resets hidden labyrinth canvas
@@ -120,22 +284,8 @@ function generateLabyrinth() {
     }
 }
 
-function redMovement(direction) { // converts the tanks movements to X and Y coordinates
-    // checks if out of bounds on X axis
-    if (redTank.x + direction * Math.sin(-redTank.rotation*Math.PI/180) < canvas.width && redTank.x + direction * Math.sin(-redTank.rotation*Math.PI/180) > 0) {
-        redTank.x += direction * Math.sin(-redTank.rotation*Math.PI/180);
-    }
-    // checks if out of bounds on Y axis   
-    if (redTank.y + direction * Math.cos(-redTank.rotation*Math.PI/180) < canvas.height && redTank.y + direction * Math.cos(-redTank.rotation*Math.PI/180) > 0) {
-        redTank.y += direction * Math.cos(-redTank.rotation*Math.PI/180);
-    }
-    // insert wall collision detection here
-}
-
 // render first frame
 generateLabyrinth();
-console.log(hWalls);
-console.log(vWalls);
 renderFrame();
 
 // renders the current frame on main canvas when called
