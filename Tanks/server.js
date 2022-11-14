1adf6b2c4acb6386efb01f36c931d9dda15ce9d0
qertend <szucslevente607@gmail.com>
2022-11-14 19:21:02 +0100

    .

--- Tanks/server.js
+++ Tanks/server.js
@@ -1,7 +1,59 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("hello");
-    res.end();
-}).listen(8080, '192.168.1.2', function() {console.log('something')});
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log('Request for ' + req.url + ' by method ' + req.method);
+
+    if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == '/') fileUrl = '/menu.html';
+        else fileUrl = req.url;
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    filePath = path.resolve('./public/404.html');
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    fs.createReadStream(filePath).pipe(res);
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            });
+        }
+        else if (fileExt == '.css') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/css');
+            fs.createReadStream(filePath).pipe(res);
+        }
+        else if (fileExt == '.js') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/javasctipt');
+            fs.createReadStream(filePath).pipe(res);
+        }
+        else {
+            filePath = path.resolve('./public/404.html');
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            fs.createReadStream(filePath).pipe(res);
+        }
+    }
+    else {
+        filePath = path.resolve('./public/404.html');
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res);
+    }
+});
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
